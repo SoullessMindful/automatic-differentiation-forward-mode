d704de53129c3d85bc9cd78ad15c071bddc9e7d6
Sławomir Kusiński <skwinged@gmail.com>
2020-01-15 15:28:34 +0100

    Datatype generalizaation

--- dual.jl
+++ dual.jl
@@ -4,29 +4,29 @@ import Base:+,-,*,/
 
 abstract type AbstractDual end
 
-struct Dual{T <: AbstractFloat} <: AbstractDual
+struct Dual{T <: Number} <: AbstractDual
     val::T
     der::T
 end
 
-Variable(a::AbstractFloat) = Dual(a, 1.)
-Constant(a::AbstractFloat) = Dual(a, 0.)
+Variable(a::Number) = Dual(a, 1.)
+Constant(a::Number) = Dual(a, 0.)
 
 a::AbstractDual + b::AbstractDual = Dual(a.val + b.val, a.der + b. der)
-a::AbstractDual + b::AbstractFloat = a + Constant(b)
-a::AbstractFloat + b::AbstractDual = b + a
+a::AbstractDual + b::Number = a + Constant(b)
+a::Number + b::AbstractDual = b + a
 
 a::AbstractDual - b::AbstractDual = Dual(a.val - b.val, a.der - b. der)
-a::AbstractDual - b::AbstractFloat = a - Constant(b)
-a::AbstractFloat - b::AbstractDual = Constant(a) - b
+a::AbstractDual - b::Number = a - Constant(b)
+a::Number - b::AbstractDual = Constant(a) - b
 
 a::AbstractDual * b::AbstractDual = Dual(a.val * b.val, a.der * b.val + a.val * b.der)
-a::AbstractDual * b::AbstractFloat = a * Constant(b)
-a::AbstractFloat * b::AbstractDual = b * a
+a::AbstractDual * b::Number = a * Constant(b)
+a::Number * b::AbstractDual = b * a
 
 a::AbstractDual / b::AbstractDual = Dual(a.val / b.val, (a.der * b.val - a.val * b.der) / (b.val * b.val))
-a::AbstractDual / b::AbstractFloat = a / Constant(b)
-a::AbstractFloat / b::AbstractDual = Constant(a) / b
+a::AbstractDual / b::Number = a / Constant(b)
+a::Number / b::AbstractDual = Constant(a) / b
 
 
 const x = Variable(1.)
